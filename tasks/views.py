d8146af3e3fd5a78a20dea9b102d0dee29e6381b
iamrizvan <rzkn000@gmail.com>
2020-10-31 07:19:25 +0400

    todo is going to live this is first commit

---
+++ tasks/views.py
@@ -0,0 +1,41 @@
+from django.http import request
+from tasks.forms import TaskForm
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+
+
+# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form, 'task': task}
+    return render(request, 'tasks/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    context = {'task': task}
+    return render(request, 'tasks/delete.html', context)
